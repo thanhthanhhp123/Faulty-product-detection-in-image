2d3665bded5c073e296643c78cfb5b625c29731f
thanhthanhhp123 <thanhmaxdz2003@gmail.com>
2024-01-19 18:42:09 +0700

    update

--- train.py
+++ train.py
@@ -1,119 +1,167 @@
-from utils import *
-from dataset import BottleDataset
-from model import UNet
-
-from torch.utils.data import DataLoader
-import torchmetrics
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
-from tqdm import tqdm
-from glob import glob
+import argparse
+import logging
 import os
+import random
+import sys
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
+from pathlib import Path
+from torch import optim
+from torch.utils.data import DataLoader, random_split
+from tqdm import tqdm
 
-train_size = 224
+#import wandb
+from evaluate import evaluate
+from unet import UNet
+from utils.data_loading import BDataset
+from utils.dice_score import dice_loss
 
-train_transforms = A.Compose(
-    [
-        A.Resize(width=train_size, height=train_size),
-        A.HorizontalFlip(),
-        A.RandomBrightnessContrast(),
-        A.Blur(),
-        A.Sharpen(),
-        A.RGBShift(),
-        A.Cutout(num_holes=5, max_h_size=25, max_w_size=25, fill_value=0),
-        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0),
-        ToTensorV2(),
-    ]
-)
+train_img = Path('./data/imgs/')
+train_mask = Path('./data/masks/')
 
-val_transforms = A.Compose(
-    [
-        A.Resize(width=train_size, height=train_size),
-        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0),
-        ToTensorV2(),
-    ]
-)
+val_img = Path('./data/imgs/')
+val_mask = Path('./data/masks/')
+dir_checkpoint = Path('./checkpoints/')
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-batch_size = 8
-n_workers = os.cpu_count()
-print(f"Using {n_workers} workers")
+def train_model(
+        model,
+        device,
+        epochs: int = 5,
+        batch_size: int = 1,
+        learning_rate: float = 1e-5,
+        val_percent: float = 0.1,
+        save_checkpoint: bool = True,
+        img_scale: float = 0.5,
+        amp: bool = False,
+        weight_decay: float = 1e-8,
+        momentum: float = 0.999,
+        gradient_clipping: float = 1.0,
+):
+    # 1. Create dataset
+    train_set = BDataset(dir_img, dir_mask, img_scale)
+    val_set = BDataset(dir_img, dir_mask, img_scale)
 
-train_loader = DataLoader(
-    BottleDataset(
-        image_dir="bottle/train/images/",
-        mask_dir="bottle/train/masks/",
-        transforms=train_transforms,
-    ),
-    batch_size=batch_size,
-    shuffle=True,
-    num_workers=n_workers,
-)
+    # 2. Split into train / validation partitions
 
-val_loader = DataLoader(
-    BottleDataset(
-        image_dir="bottle/val/images/",
-        mask_dir="bottle/val/masks/",
-        transforms=val_transforms,
-    ),
-    batch_size=batch_size,
-    shuffle=False,
-    num_workers=n_workers,
-)
+    # 3. Create data loaders
+    loader_args = dict(batch_size=batch_size, num_workers=os.cpu_count(), pin_memory=True)
+    train_loader = DataLoader(train_set, shuffle=True, **loader_args)
+    val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
 
-model = UNet(1).to(device)
+    # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
+    optimizer = optim.RMSprop(model.parameters(),
+                              lr=learning_rate, weight_decay=weight_decay, momentum=momentum, foreach=True)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=5)  # goal: maximize Dice score
+    grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
+    criterion = nn.CrossEntropyLoss() if model.n_classes > 1 else nn.BCEWithLogitsLoss()
+    global_step = 0
 
-criterion = nn.CrossEntropyLoss()
+    # 5. Begin training
+    for epoch in range(1, epochs + 1):
+        model.train()
+        epoch_loss = 0
+        with tqdm(total=n_train, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
+            for batch in train_loader:
+                images, true_masks = batch['image'], batch['mask']
 
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-n_eps = 30
+                assert images.shape[1] == model.n_channels, \
+                    f'Network has been defined with {model.n_channels} input channels, ' \
+                    f'but loaded images have {images.shape[1]} channels. Please check that ' \
+                    'the images are loaded correctly.'
 
-dice_fn = torchmetrics.Dice(eps=1e-7, average="macro").to(device)
+                images = images.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+                true_masks = true_masks.to(device=device, dtype=torch.long)
 
-iou_fn = torchmetrics.IoU(eps=1e-7, average="macro").to(device)
+                with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
+                    masks_pred = model(images)
+                    if model.n_classes == 1:
+                        loss = criterion(masks_pred.squeeze(1), true_masks.float())
+                        loss += dice_loss(F.sigmoid(masks_pred.squeeze(1)), true_masks.float(), multiclass=False)
+                    else:
+                        loss = criterion(masks_pred, true_masks)
+                        loss += dice_loss(
+                            F.softmax(masks_pred, dim=1).float(),
+                            F.one_hot(true_masks, model.n_classes).permute(0, 3, 1, 2).float(),
+                            multiclass=True
+                        )
 
-acc_meter = AverageMeter()
-train_loss_meter = AverageMeter()
-val_loss_meter = AverageMeter()
-dice_meter = AverageMeter()
-iou_meter = AverageMeter()
+                optimizer.zero_grad(set_to_none=True)
+                grad_scaler.scale(loss).backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clipping)
+                grad_scaler.step(optimizer)
+                grad_scaler.update()
 
-for epoch in range(1, 1+n_eps):
-    acc_meter.reset()
-    train_loss_meter.reset()
-    val_loss_meter.reset()
-    iou_meter.reset()
-    dice_meter.reset()
-    model.train()
+                pbar.update(images.shape[0])
+                global_step += 1
+                epoch_loss += loss.item()
+                print(f"loss: {loss.item()}")
+                # Evaluation round
+                division_step = (n_train // (5 * batch_size))
+                if division_step > 0:
+                    if global_step % division_step == 0:
+                        histograms = {}
+                        for tag, value in model.named_parameters():
+                            tag = tag.replace('/', '.')
 
-    for batch_id, (x, y,) in enumerate(
-        tqdm(train_loader, desc=f"Training epoch {epoch}", total=len(train_loader))
-    ):
-        optimizer.zero_grad()
-        n = x.shape[0]
-        x = x.to(device).float()
-        y = y.to(device).long()
-        y_pred = model(x)
-        loss = criterion(y_pred, y)
-        loss.backward()
-        optimizer.step()
+                        val_score = evaluate(model, val_loader, device, amp)
+                        scheduler.step(val_score)
 
-        with torch.no_grad():
-            y_pred_mask = y_pred.argmax(dim=1).squeeze()
-            dice_score = dice_fn(y_pred_mask, y.long())
-            iou_score = iou_fn(y_pred_mask, y.long())
-            acc = accuracy_function(y_pred_mask, y.long())
 
-        train_loss_meter.update(loss.item(), n)
-        dice_meter.update(dice_score.item(), n)
-        acc_meter.update(acc.item(), n)
-        iou_meter.update(iou_score.item(), n)
-    print(
-        f"Training epoch {epoch}: train_loss: {train_loss_meter.avg:.4f}, acc: {acc_meter.avg:.4f}, dice: {dice_meter.avg:.4f}, iou: {iou_meter.avg:.4f}"
+        if save_checkpoint:
+            Path(dir_checkpoint).mkdir(parents=True, exist_ok=True)
+            state_dict = model.state_dict()
+            state_dict['mask_values'] = dataset.mask_values
+            torch.save(state_dict, str(dir_checkpoint / 'checkpoint_epoch{}.pth'.format(epoch)))
+            logging.info(f'Checkpoint {epoch} saved!')
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
+    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
+    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
+    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5,
+                        help='Learning rate', dest='lr')
+    parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
+    parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
+    parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
+                        help='Percent of the data that is used as validation (0-100)')
+    parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
+    parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
+    parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_args()
+
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device {device}')
+
+    # Change here to adapt to your data
+    # n_channels=3 for RGB images
+    # n_classes is the number of probabilities you want to get per pixel
+    model = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
+    model = model.to(memory_format=torch.channels_last)
+
+    if args.load:
+        state_dict = torch.load(args.load, map_location=device)
+        del state_dict['mask_values']
+        model.load_state_dict(state_dict)
+
+    model.to(device=device)
+    train_model(
+        model=model,
+        epochs=args.epochs,
+        batch_size=args.batch_size,
+        learning_rate=args.lr,
+        device=device,
+        img_scale=args.scale,
+        val_percent=args.val / 100,
+        amp=args.amp
     )
-    if epoch % 5 == 0:
-        torch.save(model.state_dict(), f"bottle_model_epoch_{epoch}.pth")
-    
---
+++ utils/blending.py
@@ -0,0 +1,4 @@
+from PIL import Image
+import os 
+
+dir_path = ''
